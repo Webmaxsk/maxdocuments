4f14fc9042840fc65bc58cc2c47b5a24d720ba5d
pali ondras <ondras@webmax.sk>
2018-02-20 14:46:58 +0100

    ss4 init version

--- code/ObjectDocumentsExtension.php
+++ code/ObjectDocumentsExtension.php
@@ -1,9 +1,16 @@
 <?php
 
-class ObjectDocumentsExtension extends DataExtension {
+use SilverStripe\Core\Extension;
+use SilverStripe\Assets\File;
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+use SilverStripe\Forms\literalField;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+
+class ObjectDocumentsExtension extends Extension {
 
 	private static $allow_documents = true;
-	private static $documents_count = 20;
 	
 	private static $db = array(
 		'DocumentsContent' => 'HTMLText',
@@ -11,8 +18,12 @@ class ObjectDocumentsExtension extends DataExtension {
 	);
 	
 	private static $many_many = array(
-		'Documents' => 'File'
+		'Documents' => File::class
 	);
+
+	private static $owns = [
+		'Documents'
+	];
 	
 	private static $many_many_extraFields = array(
 		'Documents' => array('SortOrder' => 'Int')
@@ -27,36 +38,28 @@ class ObjectDocumentsExtension extends DataExtension {
 		if ($owner::config()->allow_documents) {
 			$fields->addFieldToTab('Root.Documents', HtmlEditorField::create('DocumentsContent',_t("Object.DOCUMENTSCONTENT", "Content"))->setRows(3));
 
-			$limit = $owner::config()->documents_count;
+			//$uploadClass = (class_exists("SortableUploadField") && $this->owner->DocumentsSorter == "SortOrder") ? "SortableUploadField" : "UploadField";
+			//$documentField = $uploadClass::create('Documents');
+			$documentField = UploadField::create('Documents');
 
-			$uploadClass = (class_exists("SortableUploadField") && $this->owner->DocumentsSorter == "SortOrder") ? "SortableUploadField" : "UploadField";
-
-			$documentField = $uploadClass::create('Documents');
-			$documentField->setConfig('allowedMaxFileNumber', $limit);
-			$documentField->setAllowedFileCategories('zip','doc');
-			$documentField->setAllowedExtensions(array_merge(array('zip'), $documentField->getAllowedExtensions()));
+			$documentField->setAllowedFileCategories(array('document','archive'));
 			$documentField->setFolderName('Uploads/'.$this->owner->ClassName.'/'.$this->owner->ID);
 
-			if ($limit==1) {
-				$documentsTab->setTitle(_t("Object.DOCUMENTTAB", "Document"));
-				$documentField->setTitle(_t("Object.DOCUMENTUPLOADLABEL", "Document"));
+
+			$documentsTab->setTitle(_t("Object.DOCUMENTSTAB", "Documents"));
+			$documentField->setTitle(_t("Object.DOCUMENTSUPLOADLABEL", "Documents"));
+
+			if ($this->owner->DocumentsSorter == "SortOrder")  {
+				$message = (class_exists("SortableUploadField")) ? _t("Object.DOCUMENTSUPLOADHEADING", "<span style='color: green'>Sort documents by draging thumbnail</span>") : _t("Object.DOCUMENTSUPLOADHEADINGWRONG", "<span style='color: red'>Sorting documents by draging thumbnails (SortOrder) not allowed. Missing module SortabeUploadField.</span>");
+			} else {
+				$message = _t("Object.DOCUMENTSSORTERNOTICE", "Correct document sorting is visible on frontend only (if Sort by = Title, ID)");
 			}
-			else {
-				$documentsTab->setTitle(_t("Object.DOCUMENTSTAB", "Documents"));
-				$documentField->setTitle(_t("Object.DOCUMENTSUPLOADLABEL", "Documents"));
-				$documentField->setDescription(sprintf(_t("Object.DOCUMENTSUPLOADLIMIT","Documents count limit: %s"), $limit));
 
-				if ($this->owner->DocumentsSorter == "SortOrder")  {
-					$message = (class_exists("SortableUploadField")) ? _t("Object.DOCUMENTSUPLOADHEADING", "<span style='color: green'>Sort documents by draging thumbnail</span>") : _t("Object.DOCUMENTSUPLOADHEADINGWRONG", "<span style='color: red'>Sorting documents by draging thumbnails (SortOrder) not allowed. Missing module SortabeUploadField.</span>"); 
-				} else {
-					$message = _t("Object.DOCUMENTSSORTERNOTICE", "Correct document sorting is visible on frontend only (if Sort by = Title, ID)");
-				}
+			$dropdownSorter = DropdownField::create('DocumentsSorter', _t("Object.DOCUMENTSSORTER", "Sort documents by:"))->setSource($this->owner->dbObject('DocumentsSorter')->enumValues());
+			$fields->addFieldToTab('Root.Documents', $dropdownSorter);
 
-				$dropdownSorter = DropdownField::create('DocumentsSorter', _t("Object.DOCUMENTSSORTER", "Sort documents by:"))->setSource($this->owner->dbObject('DocumentsSorter')->enumValues());
-				$fields->addFieldToTab('Root.Documents', $dropdownSorter);
+			$fields->addFieldToTab('Root.Documents', LiteralField::create('DocumentsNotice', $message));
 
-				$fields->addFieldToTab('Root.Documents', HeaderField::create('DocumentsNotice', $message)->setHeadingLevel(4));
-			}
 
 			$fields->addFieldToTab('Root.Documents', $documentField);
 		}
